83c94fe2294fb0e479ae24405884e0cf4ffa7e61
Iskelo <iskelo.kap@gmail.com>
2023-10-21 21:17:59 +0200

    123

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+/*function burgerMenu() {
+	const menu = document.querySelector('.header__menu');
+	const burger = document.querySelector('.header__burger');
+	const b = document.querySelector('body');
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('active');
+		menu.classList.toggle('active');
+		b.classList.toggle('lock');
+	})
+}
+burgerMenu();*/
